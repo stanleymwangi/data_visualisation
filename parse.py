4687ca59d9e0b73c86d94869f16728553bd32689
Stanley Mwangi <stanleym2711@gmail.com>
2015-04-22 14:29:33 +0100

    Sketched out main intermediary steps of function parse

---
+++ parse.py
@@ -0,0 +1,20 @@
+# Parse sample SF crime data from a CSV file
+import csv # obtain the csv module so that we can read csv files
+
+MY_FILE="../data/sample_sfpd_incident_all.csv" # global variable MY_FILE holds data location
+
+
+def parse(raw_file, delimeter):
+    """Parses a raw CSV file to a JSON-like object."""
+    
+    # Open CSV file
+    
+    # Read CSV file
+    
+    # Close CSV file
+    
+    # Build a data structure to return parsed data
+    
+    # Return parsed data    
+    return parsed_data 
+    
