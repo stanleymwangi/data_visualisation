54034c4dbc4ea86b91745c85b10bb25ced9d01ed
Stanley Mwangi <stanleym2711@gmail.com>
2015-04-22 15:54:40 +0100

    Renamed delimeter to delimiter in parse function header

--- source/parse.py
+++ source/parse.py
@@ -4,7 +4,7 @@ import csv # obtain the csv module so that we can read csv files
 MY_FILE="../data/sample_sfpd_incident_all.csv" # global variable MY_FILE holds data location
 
 
-def parse(raw_file, delimeter):
+def parse(raw_file, delimiter):
     """Parses a raw CSV file to a JSON-like object."""
     
     # Open CSV file
