97c3d3de10d60a1686748acf28673b8c7507abc2
Stanley Mwangi <stanleym2711@gmail.com>
2015-04-23 22:48:20 +0100

    Outlined basic structure of visualise_days

---
+++ source/graph.py
@@ -0,0 +1,31 @@
+# Take parsed data and visualize using popular Python math libraries
+from collections import Counter
+
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+
+import parse as prs 
+
+data_file = prs.parse(MY_FILE, ",")
+
+def visualize_days(parsed_data):
+    """Visualize data by day of week"""
+    
+    # Count how many incidents occur per day, store result in counter variable
+    
+    # Separate days of the week (x-axis) from number of incidents per day (y-axis)
+    
+    # Assign number of incidents per day to a matplotlib plot instance
+    
+    # Create x-axis ticks, assign the labels
+    
+    # Save the plot
+    
+    # Close the plot file
+    
+def main():
+    visualize_days(data_file)
+    
+if __name__ == "__main__":
+    main()
