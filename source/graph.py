febbbde9be5dd38b8ace941d52e61ff696725a09
Stanley Mwangi <stanleym2711@gmail.com>
2015-05-16 15:50:11 +0100

    Completed visualize_days function definition

--- source/graph.py
+++ source/graph.py
@@ -7,22 +7,38 @@ import numpy as np
 
 import parse as prs 
 
-parsed_data = prs.parse(prs.MY_FILE, ",")
+data_file = prs.parse(prs.MY_FILE, ",")
 
 def visualize_days(parsed_data):
     """Visualize data by day of week"""
     
-    # Count how many incidents occur per day, store result in counter variable
+    # Count how many incidents occur per day, store result in counter 
+    counter = Counter(item["DayOfWeek"] for item in parsed_data) # counter is a dictionary object
     
     # Separate days of the week (x-axis) from number of incidents per day (y-axis)
+    # plt.xticks() ony accepts tuples for x-axis labelling
+    week_days = tuple(["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]) 
+    # obtain the frequency of incidents for each day of the week
+    incident_count = [counter["Monday"], 
+                      counter["Tuesday"],
+                      counter["Wednesday"],
+                      counter["Thursday"], 
+                      counter["Friday"],
+                      counter["Saturday"],
+                      counter["Sunday"]]
     
     # Assign number of incidents per day to a matplotlib plot instance
-    
+    plt.plot(incident_count)
+
     # Create x-axis ticks, assign the labels
+    # range(len()) tells matplotlib how many ticks to place
+    plt.xticks(range(len(week_days)), week_days) 
     
     # Save the plot
+    plt.savefig("Days.png")
     
     # Close the plot file
+    plt.clf()
     
 def main():
     visualize_days(data_file)
